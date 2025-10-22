422fa76d3e21a2423612218a864e861552fc4bcf
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2025-10-22 21:58:24 +0300

    chore: added ymaps3 default ui + trycloudflare.com allowedhosts

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,20 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://odnokreslo.ru',
   integrations: [sitemap()],
+  vite: {
+    server: {
+      allowedHosts: [
+        'localhost',
+        '.trycloudflare.com',
+      ],
+    },
+    build: {
+      target: 'es2022',
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        target: 'es2022',
+      },
+    },
+  },
 });
