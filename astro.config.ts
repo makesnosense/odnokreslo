9a7b29d773871ae54b70bf6d04370177250c0cab
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2026-02-09 18:12:04 +0300

    chore: add bundle visualizer and bump ymaps3-default-ui version

--- astro.config.ts
+++ astro.config.ts
@@ -1,26 +1,30 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-  site: 'https://odnokreslo.ru',
+  site: "https://odnokreslo.ru",
   integrations: [sitemap()],
   vite: {
+    plugins: [
+      visualizer({
+        open: true,
+        filename: "dist/stats.html",
+      }),
+    ],
     server: {
-      allowedHosts: [
-        'localhost',
-        '.trycloudflare.com',
-      ],
+      allowedHosts: ["localhost", ".trycloudflare.com"],
     },
     build: {
-      target: 'es2022',
+      target: "es2022",
     },
     optimizeDeps: {
       esbuildOptions: {
-        target: 'es2022',
+        target: "es2022",
       },
     },
   },
   devToolbar: {
-    enabled: false
-  }
+    enabled: false,
+  },
 });
